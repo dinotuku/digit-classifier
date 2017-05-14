44f9297037382ff7a6757c34f0d44d96f040a06d
Kuan Tung <b03901039@ntu.edu.tw>
2017-05-14 16:03:47 +0800

    Predict working

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ $(() => {
     controls: [
       'Navigation',
     ],
+    enlargeYourContainer: true,
   });
   const model = new KerasJS.Model({
     filepaths: {
@@ -14,10 +15,45 @@ $(() => {
     filesystem: true,
   });
   const $submitBtn = $('.submit-btn');
+  const $showResult = $('.classify-result');
+  let result;
 
   $submitBtn.click(() => {
     const ctx = drawBoard.ctx;
-    ctx.scale(0.1, 0.1);
-    console.log(ctx.getImageData(0, 0, 28, 28));
+
+    // Scaled to 28 x 28
+    const ctxScaled = document.getElementById('input-canvas-scaled').getContext('2d');
+    ctxScaled.save();
+    ctxScaled.scale(28 / ctx.canvas.width, 28 / ctx.canvas.height);
+    ctxScaled.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctxScaled.drawImage(ctx.canvas, 0, 0);
+    const imageDataScaled = ctxScaled.getImageData(0, 0,
+      ctxScaled.canvas.width, ctxScaled.canvas.height);
+    ctxScaled.restore();
+
+    // Process image data for model input
+    const { data } = imageDataScaled;
+    this.input = new Float32Array(784);
+    for (let i = 0, len = data.length; i < len; i += 4) {
+      this.input[i / 4] = ((data[i] / 255) - 1) * -1;
+    }
+
+    model.ready()
+      .then(() => {
+        model.predict({ input: this.input })
+          .then((outputData) => {
+            result = outputData.output;
+            console.log(result);
+            console.log(result.indexOf(Math.max(...result)));
+            $showResult.html(`It should be a ${result.indexOf(Math.max(...result))}.`);
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+        return null;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   });
 });
